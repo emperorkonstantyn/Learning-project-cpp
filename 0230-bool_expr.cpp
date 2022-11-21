3cf9465ead29dcbc2137477ca84aaebed1c0a406
emperorkonstantyn <emperorkmr@gmail.com>
2022-11-21 20:43:26 +0200

    0230-bool_expr.cpp added

---
+++ 0230-bool_expr.cpp
@@ -0,0 +1,14 @@
+﻿// bool_expr.cpp
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+  double x = 0;
+  cout << "x = ";
+  cin >> x;
+  // Логическое выражение.
+  cout << "x*x < 2  == " << (x*x < 2) << endl;
+  return EXIT_SUCCESS;
+}
