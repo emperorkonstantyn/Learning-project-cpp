909c5d157f133523844b648741247bbdacae9b35
emperorkonstantyn <39656501+emperorkonstantyn@users.noreply.github.com>
2022-11-30 14:13:26 +0200

    Add files via upload

---
+++ 0086-russian_win32_cpp.cpp
@@ -0,0 +1,11 @@
+﻿// russian_win32_cpp.cpp
+#include <iostream>
+#include <locale>
+
+int main()
+{
+  using namespace std;
+  locale::global(locale("Russian"));
+  cout << "Текст" << endl;
+  return EXIT_SUCCESS;
+}
